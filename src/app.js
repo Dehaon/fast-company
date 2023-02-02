fe064f4c56e9e9b24258ecb53372410d0b4ea7aa
Denis Korolev <deniss92kor@gmail.com>
2023-02-02 08:23:24 +0530

    added an asynchronous request for user data, changed the comparison method in the filter

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Users from "./components/users";
 import api from "./api/index";
 
 function App() {
-  const [users, setUsers] = useState(api.users.fetchAll());
-  //   console.log(users);
+  const [users, setUsers] = useState();
+
+  useEffect(() => {
+    api.users.fetchAll().then((data) => setUsers(data));
+  }, []);
 
   const handleDelete = (userId) => {
     const newUsers = users.filter((user) => user._id !== userId);
@@ -24,11 +27,13 @@ function App() {
 
   return (
     <>
-      <Users
-        users={users}
-        onDeleteButtonClick={handleDelete}
-        onBookmarkIconClick={handleToggleBookmark}
-      />
+      {users && (
+        <Users
+          users={users}
+          onDeleteButtonClick={handleDelete}
+          onBookmarkIconClick={handleToggleBookmark}
+        />
+      )}
     </>
   );
 }
