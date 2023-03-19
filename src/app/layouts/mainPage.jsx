55231892f9d183ecd0c765c2332ad0352e87059b
Denis Korolev <deniss92kor@gmail.com>
2023-03-19 15:04:32 +0530

    added login form

--- src/app/layouts/mainPage.jsx
+++ src/app/layouts/mainPage.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const MainPage = () => {
-  return <h1>Главная страница</h1>;
+  return (
+    <>
+      <h1>Главная страница</h1>
+    </>
+  );
 };
 
 export default MainPage;
