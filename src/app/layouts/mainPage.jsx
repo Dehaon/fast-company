132676100c41548f657d5a60d6717f7a2883bdf0
Denis Korolev <deniss92kor@gmail.com>
2023-06-02 12:45:25 +0600

    migrated to Firebase

--- src/app/layouts/mainPage.jsx
+++ src/app/layouts/mainPage.jsx
@@ -1,10 +1,26 @@
 import React from "react";
+import useMockData from "../utils/mockData";
 
 const MainPage = () => {
+  const { errors, initialize, progress, status } = useMockData();
+
+  const handleClick = () => {
+    console.log("click");
+    initialize();
+  };
   return (
-    <>
+    <div className="container mt-5">
       <h1>Главная страница</h1>
-    </>
+      <h3>Инициализация данных в Firebase</h3>
+      <ul>
+        <li>Status: {status}</li>
+        <li>Progress: {progress}%</li>
+        {errors && <li>Error: {errors}</li>}
+      </ul>
+      <button className="btn btn-primary" onClick={handleClick}>
+        Init
+      </button>
+    </div>
   );
 };
 
